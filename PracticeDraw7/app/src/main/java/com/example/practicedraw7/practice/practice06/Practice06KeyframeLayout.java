30800795a56423386bcc05c47b10e62f2e2d6c73
huyu <2356048622@qq.com>
2021-03-18 20:55:29 +0800

    feat:PracticeDraw

---
+++ PracticeDraw7/app/src/main/java/com/example/practicedraw7/practice/practice06/Practice06KeyframeLayout.java
@@ -0,0 +1,56 @@
+package com.example.practicedraw7.practice.practice06;
+
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import androidx.constraintlayout.motion.widget.KeyFrames;
+
+import com.example.practicedraw7.R;
+
+public class Practice06KeyframeLayout extends RelativeLayout {
+    Practice06KeyframeView view;
+    Button animateBt;
+
+    public Practice06KeyframeLayout(Context context) {
+        super(context);
+    }
+
+    public Practice06KeyframeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice06KeyframeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        view = (Practice06KeyframeView) findViewById(R.id.objectAnimatorView);
+        animateBt = (Button) findViewById(R.id.animateBt);
+
+        animateBt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 使用 Keyframe.ofFloat() 来为 view 的 progress 属性创建关键帧
+                // 初始帧：progress 为 0
+                // 时间进行到一般：progress 为 100
+                // 结束帧：progress 回落到 80
+                // 使用 PropertyValuesHolder.ofKeyframe() 来把关键帧拼接成一个完整的属性动画方案
+                // 使用 ObjectAnimator.ofPropertyValuesHolder() 来创建动画
+                Keyframe startFrame = Keyframe.ofFloat(0, 0);
+                Keyframe midFrame = Keyframe.ofFloat(0.8f, 100);
+                Keyframe endFrame = Keyframe.ofFloat(1f, 80);
+                PropertyValuesHolder propertyValuesHolder = PropertyValuesHolder.ofKeyframe("progress", startFrame, midFrame, endFrame);
+                ObjectAnimator.ofPropertyValuesHolder(view, propertyValuesHolder).setDuration(2000).start();
+            }
+        });
+    }
+}
